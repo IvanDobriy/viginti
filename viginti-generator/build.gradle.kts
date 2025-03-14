1c2228ce290ba00335ca28446bfe95b4cad68929
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-03-14 22:58:50 +0300

    prepare generator

--- viginti-generator/build.gradle.kts
+++ viginti-generator/build.gradle.kts
@@ -1,2 +1,7 @@
 project.group = "com.rid.virginti"
-project.version = extensions.getByType<VersionCatalogsExtension>().named("libs").findVersion("virgintiVersion").get().toString()
+project.version =
+    extensions.getByType<VersionCatalogsExtension>().named("libs").findVersion("virgintiVersion").get().toString()
+
+plugins {
+    java
+}
