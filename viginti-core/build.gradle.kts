0248ac3e7918c72f7c49105347927c6b64da69c3
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-03-14 22:49:50 +0300

    prepare core

--- viginti-core/build.gradle.kts
+++ viginti-core/build.gradle.kts
@@ -1,2 +1,8 @@
 project.group = "com.rid.virginti"
 project.version = extensions.getByType<VersionCatalogsExtension>().named("libs").findVersion("virgintiVersion").get().toString()
+
+
+plugins{
+    `java-library`
+}
+
---
+++ viginti-core/src/main/java/com/rid/virginti/core/Property.java
@@ -0,0 +1,6 @@
+package com.rid.virginti.core;
+
+public interface Property<T> {
+    T get();
+    void set(T value);
+}
